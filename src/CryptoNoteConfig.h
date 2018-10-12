5465523b9ec78b63677453c8401ce4ada1b0f19a
zephyr0xdev <44080369+zephyr0xdev@users.noreply.github.com>
2018-10-12 07:57:35 +0200

    Update CryptoNoteConfig.h
    updated the genblock hash

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "zephyr0x";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001a025029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101982c48bf4b53e40275ee8b8611e3cfd753ccf3367e2122424149340f9da5db52";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
